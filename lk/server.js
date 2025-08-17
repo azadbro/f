4fc3b609433b4e0d3088e161f55be31fe4bff955
azadbro <sadre7348@gmail.com>
2025-08-17 19:00:29 +0530

    Update server.js

--- lk/server.js
+++ lk/server.js
@@ -7,7 +7,7 @@ const crypto = require('crypto');
 require('dotenv').config({ path: './config.env' });
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
